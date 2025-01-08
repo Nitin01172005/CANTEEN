175bb3ae1c4ea8ff3c0e6db8cfaf8b4975320fa1
NITIN KIROLA <nitinkirola@NITINs-MacBook-Air.local>
2025-01-08 20:10:47 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AOS from 'aos';
-import Home from './components/Home'
-import Navbar from './components/Navbar'
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 import Products from './components/Products';
 import TopProducts from './components/TopProducts';
 import Banner from './components/Banner';
@@ -9,20 +9,18 @@ import Subscribe from './components/Subscribe';
 import Testionials from './components/Testionials';
 import Footer from './components/Footer';
 import Popup from './components/Popup';
-import Subscription from './components/Subscription'
+import Subscription from './components/Subscription';
 import About from './components/About';
 import Mid from './components/Mid';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
-
-
-
   const [orderPopup, setOrderPopup] = React.useState(false);
 
   const handleOrderPopup = () => {
     setOrderPopup(!orderPopup);
-  }
+  };
 
   React.useEffect(() => {
     AOS.init({
@@ -32,28 +30,48 @@ function App() {
       delay: 100,
     });
     AOS.refresh();
-  },[])
+  }, []);
 
   return (
-    <>
-      <div className='bg-gray-900'>
-      <Navbar />
-      <About />
-      <Home handleOrderPopup = {handleOrderPopup} />
-      <Products handleOrderPopup = {handleOrderPopup}  />
-      <TopProducts handleOrderPopup = {handleOrderPopup}  />
-      <Banner />
-      <Subscription />
-      <Subscribe />
-      <Mid />
+    <BrowserRouter>
+      {/* Main Layout Components */}
+      <div className="bg-gray-900">
+        {/* Main Content (visible on all pages except Login) */}
+        <Navbar />
+        <Routes>
+          {/* Route for the login page */}
+          <Route path="/login" element={<Login />} />
 
-      <Testionials />
+          {/* Default routes for other components */}
+          <Route
+            path="/"
+            element={
+              <>
+                <About />
+                <Home handleOrderPopup={handleOrderPopup} />
+                <Products handleOrderPopup={handleOrderPopup} />
+                <TopProducts handleOrderPopup={handleOrderPopup} />
+                <Banner />
+                <Subscription />
+                <Subscribe />
+                <Mid />
 
-      <Footer />
-      <Popup  orderPopup={orderPopup} setOrderPopup={setOrderPopup}  />
-       </div>
-    </>
-  )
+        <Testionials />
+        <Footer />
+        <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+              </>
+            }
+          />
+          {/* Catch-all route for non-existent routes */}
+      
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+// Login Component
+
+
+export default App;
